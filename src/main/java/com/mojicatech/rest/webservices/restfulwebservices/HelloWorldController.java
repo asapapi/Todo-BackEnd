a38f9f63bc59601e9a2128e99a5b8b1290b2853f
Asahi <asahimojica@yahoo.com>
2020-07-03 11:33:46 -0500

    connection between angular and spring establish

--- src/main/java/com/mojicatech/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/mojicatech/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.mojicatech.rest.webservices.restfulwebservices;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
