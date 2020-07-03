0d3dcbdf54cb840971171feb02492229b3cc0030
Asahi <asahimojica@yahoo.com>
2020-07-02 23:01:23 -0500

    Testing helloworld

---
+++ src/main/java/com/mojicatech/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.mojicatech.rest.webservices.restfulwebservices;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return ("Hello World");
+    }
+
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello world");
+    }
+    //Get
+    //URI
+    //method - "Hello World"
+}
